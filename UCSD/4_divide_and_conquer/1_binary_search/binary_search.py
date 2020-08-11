8c9774287b31e1a0f5b072c9526d1acdfbf325c9
Binil Kuriachan <bkuriach@asu.edu>
2020-08-11 12:13:05 +0530

    Committing Binary Search - Divide and Conquer

--- UCSD/4_divide_and_conquer/1_binary_search/binary_search.py
+++ UCSD/4_divide_and_conquer/1_binary_search/binary_search.py
@@ -1,9 +1,30 @@
 # Uses python3
 import sys
 
-def binary_search(a, x):
-    left, right = 0, len(a)
-    # write your code here
+# def binary_search(a, x):
+#     left, right = 0, len(a)
+#     if right >= 1:
+#         mid = left + (right-1)//2
+#         if a[mid] == x:
+#             return mid
+#         elif x > a[mid]:
+#             return binary_search(a[mid+1:], x)
+#         else:
+#             return binary_search(a[0:mid-1], x)
+#     else:
+#         return -1
+
+def binary_search(a, left, right, x):
+    if left <= right:
+        mid = left + (right-left)//2
+        if a[mid] == x:
+            return mid
+        elif x > a[mid]:
+            return binary_search(a, mid+1, right, x)
+        else:
+            return binary_search(a,0,mid-1, x)
+    else:
+        return -1
 
 def linear_search(a, x):
     for i in range(len(a)):
@@ -19,4 +40,5 @@ if __name__ == '__main__':
     a = data[1 : n + 1]
     for x in data[n + 2:]:
         # replace with the call to binary_search when implemented
-        print(linear_search(a, x), end = ' ')
+        # print(linear_search(a, x), end = ' ')
+        print(binary_search(a, 0, len(a)-1, x), end=' ')
